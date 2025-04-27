7940dee83d592c4e6ca8f37078801d5b41c27a19
Vineet Kumar Singh <vineetkr769@gmail.com>
2025-04-27 15:55:20 +0530

    second commit

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,9 +3,11 @@ import React from 'react';
 import { IncidentDashboard } from '@/components/IncidentDashboard';
 
 const Index = () => {
+  //dsvhnsvh
   return (
     <div className="min-h-screen bg-gradient-to-b from-gray-900 to-gray-800 text-gray-100">
       <IncidentDashboard />
+      
     </div>
   );
 };
